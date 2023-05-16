3cad80d6ba2d4d69d55d3bb2bbe08a9f377b5b33
SyuA0529 <silbi3408@gmail.com>
2023-05-17 02:35:02 +0900

    Init: initailize Spring Boot Project

---
+++ src/main/java/com/dku/cloudcomputing/CloudcomputingApplication.java
@@ -0,0 +1,13 @@
+package com.dku.cloudcomputing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcomputingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcomputingApplication.class, args);
+	}
+
+}
